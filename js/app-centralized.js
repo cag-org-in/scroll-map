f6bbb33f8347328717b34ffb29a905af35a20486
Nick Doiron <ndoiron@mapmeld.com>
2017-10-19 10:11:52 -0600

    add alternative maps

---
+++ js/app-centralized.js
@@ -0,0 +1,261 @@
+$(document).foundation();
+
+L.mapbox.accessToken = 'pk.eyJ1IjoibmRvaXJvbjIiLCJhIjoiY2o0ZzkzamN0MDFnNTMycW53cWxocjZmeiJ9.JromgSz8YMJodPRTanfspQ';
+
+// Create a map in the div #map and takes two arguments: the id of the html element and the map id from Mapbox
+var map = L.mapbox.map('map', {
+  zoomControl: false,
+  tiles: ['//api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}@2x?access_token=' + L.mapbox.accessToken]
+}).setView([13.046316617071808, 80.23306846618654], 15);
+
+// L.mapbox.styleLayer('mapbox://styles/mapbox/light-v9').addTo(map);
+
+// Create a featurelayer for markers and add to map
+var spots = L.mapbox.featureLayer()
+  .addTo(map);
+
+// Disable the zoom on scroll
+map.scrollWheelZoom.disable();
+
+// Array of story section elements.
+var sections = $('section');
+var narrative = $("#narrative")[0];
+var currentId = 'cover';
+
+function setCustomMarkers() {
+  var places = {
+    type: 'FeatureCollection',
+    features: [{
+      geometry: {
+        type: "Point",
+        coordinates: [13.046316617071808, 80.23306846618654]
+      },
+      properties: {
+        id: "cover",
+        zoom: 15
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.042837, 80.231571]
+      },
+      properties: {
+        id: "4693-2",
+        icon: "icon2"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.040602, 80.233539]
+      },
+      properties: {
+        id: "5226",
+        icon: "icon4"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.040377, 80.233607]
+      },
+      properties: {
+        id: "5226",
+        icon: "icon4"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.042744, 80.231779]
+      },
+      properties: {
+        id: "5266",
+        icon: "icon8"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.052466, 80.240058]
+      },
+      properties: {
+        id: "828",
+        icon: "icon6"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.134704, 80.269724]
+      },
+      properties: {
+        id: "842",
+        icon: "icon7"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.015966, 80.232077]
+      },
+      properties: {
+        id: "4434-2",
+        icon: "icon1"
+      },
+      type: 'Feature'
+    }]
+  };
+
+  spots.setGeoJSON(places);
+
+  // define icons as cssIcons
+  spots.eachLayer(function(e) {
+    var className = 'sprite sprite-' + e.feature.properties.id + ' ' + e.feature.properties.icon;
+    var coordinates = e.feature.geometry.coordinates;
+    var html = "<div data-id='" + e.feature.properties.id + "'></div>"
+    var cssIcon = L.divIcon({
+      // Specify a class name we can refer to in CSS.
+      className: className,
+      // Set marker width and height
+      iconSize: [80, 80],
+      html: html
+    });
+    if (e.feature.properties.id === "about") {
+      L.marker(coordinates, {
+        icon: cssIcon
+      }).addTo(map).bindPopup('<h1> Youth Radio Interactive HQ </h1>')
+    } else {
+      L.marker(coordinates, {
+        icon: cssIcon
+      }).addTo(map);
+    }
+  })
+}
+
+
+
+
+function setId(newId) {
+  // If ID hasn't change, do nothing
+  if (newId === currentId) return;
+  if (newId === 'cover') {
+    $('body').attr('class', 'section-0');
+  } else {
+    $('body').attr('class', ' ');
+  }
+  // otherwise, iterate thru layers, setting the current marker to a different style and zooming to it
+  spots.eachLayer(function(layer) {
+    if (layer.feature.properties.id === newId) {
+      var coordinates = layer.feature.geometry.coordinates;
+      var all_el = $(".sprite");
+      var el = $("div[class*='" + newId + "']")[0];
+      all_el.removeClass("active");
+      el.className = el.className + " active";
+      if (newId === "cover") {
+        map.setView(coordinates, 15);
+      } else {
+        map.setView(coordinates, 16);
+      }
+    }
+  });
+  // highlight the current section
+  for (var i = 0; i < sections.length; i++) {
+    sections[i].className = sections[i].id === newId ? 'active' : '';
+  }
+  // And then set the new id as the current one,
+  // so that we know to do nothing at the beginning
+  // of this function if it hasn't changed between calls
+  currentId = newId;
+}
+
+
+narrative.onscroll = function() {
+  var narrativeHeight = narrative.offsetHeight;
+  var newId = currentId;
+  // Find the section that's currently scrolled-to.
+  // We iterate backwards here so that we find the topmost one.
+  for (var i = sections.length - 1; i >= 0; i--) {
+    var rect = sections[i].getBoundingClientRect();
+    if (rect.top >= 0 && rect.top / .5 <= narrativeHeight) {
+      newId = sections[i].id;
+    }
+  }
+  setId(newId);
+};
+
+function markersOnClik() {
+  $(".sprite").on("click", function() {
+    var data = $(this).children().data().id
+    var id = "#" + data
+    window.location.hash = id
+    $("section").removeClass("active")
+    $(id).addClass("active")
+    setId(data);
+    event.preventDefault();
+  });
+}
+
+
+
+// SPLASH SCREEN
+
+/* fix vertical when not overflow
+call fullscreenFix() if .fullscreen content changes */
+function fullscreenFix() {
+  var h = $('body').height();
+  // set .fullscreen height
+  $(".content-b").each(function() {
+    if ($(this).innerHeight() <= h) {
+      $(this).closest(".fullscreen").addClass("not-overflow");
+    }
+  });
+}
+
+
+
+function fadeOnClick() {
+  var target = $("#splash");
+  var target_two = $("#main-content-wrapper")
+  $(".cust-button").on("click", function(e) {
+    e.preventDefault()
+    target.fadeOut("slow");
+    target_two.addClass("visible");
+  });
+}
+
+function animateLogo(){
+  var target = $("#logo")
+  setTimeout(function(){
+    target.addClass("show"); }, 500);
+  setTimeout(function(){
+    target.fadeIn();
+    target.addClass("move-left");
+    $(".target").addClass("show");
+  }, 1500);
+}
+
+function initSoc(){
+/*
+  new Share(".share-bttn", {
+  networks: {
+    facebook: {
+      app_id: "459957714160273"
+      },
+      twitter: {
+        description:"West Side Stories: A New Interactive From Youth Radio Interactive via @youthradio."
+      }
+    }
+  });
+*/
+}
+
+initSoc()
+animateLogo()
+setCustomMarkers()
+markersOnClik()
+setId('cover');
+fadeOnClick();
+$(window).resize(fullscreenFix);
+fullscreenFix();
---
+++ js/app-decentralized.js
@@ -0,0 +1,281 @@
+$(document).foundation();
+
+L.mapbox.accessToken = 'pk.eyJ1IjoibmRvaXJvbjIiLCJhIjoiY2o0ZzkzamN0MDFnNTMycW53cWxocjZmeiJ9.JromgSz8YMJodPRTanfspQ';
+
+// Create a map in the div #map and takes two arguments: the id of the html element and the map id from Mapbox
+var map = L.mapbox.map('map', {
+  zoomControl: false,
+  tiles: ['//api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}@2x?access_token=' + L.mapbox.accessToken]
+}).setView([13.037808622274607, 80.26373147964479], 15);
+
+// L.mapbox.styleLayer('mapbox://styles/mapbox/light-v9').addTo(map);
+
+// Create a featurelayer for markers and add to map
+var spots = L.mapbox.featureLayer()
+  .addTo(map);
+
+// Disable the zoom on scroll
+map.scrollWheelZoom.disable();
+
+// Array of story section elements.
+var sections = $('section');
+var narrative = $("#narrative")[0];
+var currentId = 'cover';
+
+function setCustomMarkers() {
+  var places = {
+    type: 'FeatureCollection',
+    features: [{
+      geometry: {
+        type: "Point",
+        coordinates: [13.037808622274607, 80.26373147964479]
+      },
+      properties: {
+        id: "cover",
+        zoom: 15
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.041082, 80.261127]
+      },
+      properties: {
+        id: "4693-2",
+        icon: "icon2"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.034558, 80.268686]
+      },
+      properties: {
+        id: "5226",
+        icon: "icon4"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.036814, 80.268498]
+      },
+      properties: {
+        id: "5266",
+        icon: "icon8"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.037951, 80.266098]
+      },
+      properties: {
+        id: "828",
+        icon: "icon6"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.037471, 80.265355]
+      },
+      properties: {
+        id: "842",
+        icon: "icon7"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.040397, 80.263828]
+      },
+      properties: {
+        id: "4434-2",
+        icon: "icon1"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.035507, 80.217711]
+      },
+      properties: {
+        id: "851",
+        icon: "icon3"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [13.134704, 80.269724]
+      },
+      properties: {
+        id: "819",
+        icon: "icon5"
+      },
+      type: 'Feature'
+    }, {
+      geometry: {
+        type: "Point",
+        coordinates: [12.955798, 80.225809]
+      },
+      properties: {
+        id: "819",
+        icon: "icon5"
+      },
+      type: 'Feature'
+    }]
+  };
+
+  spots.setGeoJSON(places);
+
+  // define icons as cssIcons
+  spots.eachLayer(function(e) {
+    var className = 'sprite sprite-' + e.feature.properties.id + ' ' + e.feature.properties.icon;
+    var coordinates = e.feature.geometry.coordinates;
+    var html = "<div data-id='" + e.feature.properties.id + "'></div>"
+    var cssIcon = L.divIcon({
+      // Specify a class name we can refer to in CSS.
+      className: className,
+      // Set marker width and height
+      iconSize: [80, 80],
+      html: html
+    });
+    if (e.feature.properties.id === "about") {
+      L.marker(coordinates, {
+        icon: cssIcon
+      }).addTo(map).bindPopup('<h1> Youth Radio Interactive HQ </h1>')
+    } else {
+      L.marker(coordinates, {
+        icon: cssIcon
+      }).addTo(map);
+    }
+  })
+}
+
+
+
+
+function setId(newId) {
+  // If ID hasn't change, do nothing
+  if (newId === currentId) return;
+  if (newId === 'cover') {
+    $('body').attr('class', 'section-0');
+  } else {
+    $('body').attr('class', ' ');
+  }
+  // otherwise, iterate thru layers, setting the current marker to a different style and zooming to it
+  spots.eachLayer(function(layer) {
+    if (layer.feature.properties.id === newId) {
+      var coordinates = layer.feature.geometry.coordinates;
+      var all_el = $(".sprite");
+      var el = $("div[class*='" + newId + "']")[0];
+      all_el.removeClass("active");
+      el.className = el.className + " active";
+      if (newId === "cover") {
+        map.setView(coordinates, 15);
+      } else {
+        map.setView(coordinates, 16);
+      }
+    }
+  });
+  // highlight the current section
+  for (var i = 0; i < sections.length; i++) {
+    sections[i].className = sections[i].id === newId ? 'active' : '';
+  }
+  // And then set the new id as the current one,
+  // so that we know to do nothing at the beginning
+  // of this function if it hasn't changed between calls
+  currentId = newId;
+}
+
+
+narrative.onscroll = function() {
+  var narrativeHeight = narrative.offsetHeight;
+  var newId = currentId;
+  // Find the section that's currently scrolled-to.
+  // We iterate backwards here so that we find the topmost one.
+  for (var i = sections.length - 1; i >= 0; i--) {
+    var rect = sections[i].getBoundingClientRect();
+    if (rect.top >= 0 && rect.top / .5 <= narrativeHeight) {
+      newId = sections[i].id;
+    }
+  }
+  setId(newId);
+};
+
+function markersOnClik() {
+  $(".sprite").on("click", function() {
+    var data = $(this).children().data().id
+    var id = "#" + data
+    window.location.hash = id
+    $("section").removeClass("active")
+    $(id).addClass("active")
+    setId(data);
+    event.preventDefault();
+  });
+}
+
+
+
+// SPLASH SCREEN
+
+/* fix vertical when not overflow
+call fullscreenFix() if .fullscreen content changes */
+function fullscreenFix() {
+  var h = $('body').height();
+  // set .fullscreen height
+  $(".content-b").each(function() {
+    if ($(this).innerHeight() <= h) {
+      $(this).closest(".fullscreen").addClass("not-overflow");
+    }
+  });
+}
+
+
+
+function fadeOnClick() {
+  var target = $("#splash");
+  var target_two = $("#main-content-wrapper")
+  $(".cust-button").on("click", function(e) {
+    e.preventDefault()
+    target.fadeOut("slow");
+    target_two.addClass("visible");
+  });
+}
+
+function animateLogo(){
+  var target = $("#logo")
+  setTimeout(function(){
+    target.addClass("show"); }, 500);
+  setTimeout(function(){
+    target.fadeIn();
+    target.addClass("move-left");
+    $(".target").addClass("show");
+  }, 1500);
+}
+
+function initSoc(){
+/*
+  new Share(".share-bttn", {
+  networks: {
+    facebook: {
+      app_id: "459957714160273"
+      },
+      twitter: {
+        description:"West Side Stories: A New Interactive From Youth Radio Interactive via @youthradio."
+      }
+    }
+  });
+*/
+}
+
+initSoc()
+animateLogo()
+setCustomMarkers()
+markersOnClik()
+setId('cover');
+fadeOnClick();
+$(window).resize(fullscreenFix);
+fullscreenFix();
